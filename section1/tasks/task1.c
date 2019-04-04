cbdfda877a08ea44c4b4227425fc8ee7b66ab468
Sherif MEIMARI <sherifmeimari@gmail.com>
2019-04-04 14:39:12 +0200

    Update task1.c

--- section1/tasks/task1.c
+++ section1/tasks/task1.c
@@ -1,3 +1,10 @@
+// Make a function that takes an input and returns all the loop concerned except the reminder of number: 
+// Ex: 
+// Please enter the number: 50 
+// Please enter the skipped: 3 
+// The formula: 
+// 1 2 4 5 7 8 10 11 13 14 16 17 19 20......  Up to 50
+
 #include <stdio.h>
 #include <stdlib.h>
 
