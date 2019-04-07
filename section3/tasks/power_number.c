f866eb1a893e3cef2ebb8e52eb27db89bb2d4a7b
Sherif MEIMARI <sherifmeimari@gmail.com>
2019-04-07 14:39:57 +0200

    Create power_number.c

---
+++ section3/tasks/power_number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void power() {
+    int x;
+    printf("Please enter your number:\n");
+    scanf("%i", &x);
+    printf("The power of the number is:\n")
+    printf("%i", x*x);
+}
