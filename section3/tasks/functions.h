9b56865fb5c2be6285b746e15ce7ecea359bc4da
Sherif MEIMARI <sherifmeimari@gmail.com>
2019-04-07 14:41:56 +0200

    Create functions.h

---
+++ section3/tasks/functions.h
@@ -0,0 +1,19 @@
+#define MAX_LENGTH 5
+#define MAX_STACK_LENGTH 10
+#define True 1
+#define FULL "Battery Full"
+#define MOTAFAGERA "Battery MOTAFAGERA HAZEENA!"
+#define NORMAL "Battery Normal"
+#define LOW "Battery low"
+
+void create_flip_array();
+void calculator();
+void cinema_reservation();
+void miss_step();
+void battery_checker();
+void fibonacci();
+void stack();
+void power();
+void christmas_tree();
+
+void advanced_calculator();
